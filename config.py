a7fe32aac9fd172198a5bef36932f836bf2f5106
denis krishchuk <ms.den.best@mail.ru>
2024-01-14 03:24:54 +0500

    add API classes

---
+++ config.py
@@ -0,0 +1,2 @@
+HH_URL = "https://api.hh.ru/vacancies"
+SJ_URL = "https://api.superjob.ru/2.0/vacancies"
---
+++ src/api.py
@@ -0,0 +1,45 @@
+import os
+from abc import ABC, abstractmethod
+
+import requests
+from dotenv import load_dotenv
+
+from config import HH_URL, SJ_URL
+
+
+load_dotenv()
+
+
+class API(ABC):
+    @abstractmethod
+    def get_data(self) -> list[dict]:
+        pass
+
+
+class HeadHunterAPI(API):
+    def __init__(self, query: str) -> None:
+        self.query = query
+        self.params = {
+            "text": self.query,
+            "per_page": 100
+        }
+
+    def get_data(self, page=0) -> list[dict]:
+        self.params["page"] = page
+        return requests.get(url=HH_URL, params=self.params).json()["items"]
+
+
+class SuperJobAPI(API):
+    def __init__(self, query: str) -> None:
+        self.query = query
+        self.params = {
+            "keyword": self.query,
+            "count": 100
+        }
+
+    def get_data(self, page=0) -> list[dict]:
+        self.params["page"] = page
+        headers = {
+            "X-Api-App-Id": os.getenv("SJ_API_KEY")
+        }
+        return requests.get(url=SJ_URL, params=self.params, headers=headers).json()["objects"]
