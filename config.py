8ede01fd221d82ea6d83b693ced593f75ba74792
denis krishchuk <ms.den.best@mail.ru>
2024-01-16 00:45:55 +0500

    fix path to JSON file

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ from pathlib import Path
 HH_URL = "https://api.hh.ru/vacancies"
 SJ_URL = "https://api.superjob.ru/2.0/vacancies"
 
-PATH_TO_JSON = Path(__file__).parent
+PARENT_PATH = Path(__file__).parent
+PATH_TO_JSON = Path(PARENT_PATH, "vacancies.json")
