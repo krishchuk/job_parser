a18b3c1fb68d35a18439c90c1009c1c75b46b629
denis krishchuk <ms.den.best@mail.ru>
2024-01-16 00:48:40 +0500

    add funcs for read and write JSON file

--- src/files.py
+++ src/files.py
@@ -8,6 +8,7 @@ class FileSaver(ABC):
     """Абстрактный класс для работы с файлом"""
     def __init__(self, path):
         self.path = path
+        self.write_json([])
 
     @abstractmethod
     def add_vacancies(self, vacancies: list[Vacancy]) -> None:
@@ -21,21 +22,27 @@ class FileSaver(ABC):
     def del_vacancies(self, queries=None) -> None:
         pass
 
+    def read_json(self) -> list[dict]:
+        with open(self.path, "r", encoding="utf-8") as file:
+            vacancies: list[dict] = json.load(file)
+        return vacancies
+
+    def write_json(self, vacancies: list) -> None:
+        with open(self.path, "w", encoding="utf-8") as file:
+            json.dump(vacancies, file)
+
 
 class FileSaverJSON(FileSaver):
     """Класс для работы с файлом JSON"""
 
     def add_vacancies(self, vacancies: list[Vacancy]) -> None:
         vacancies_json = [vacancy.write_to_dict() for vacancy in vacancies]
-        with open(self.path, "r", encoding="utf-8") as file:
-            old_vacancies: list[dict] = json.load(file)
+        old_vacancies = self.read_json()
         old_vacancies.extend(vacancies_json)
-        with open(self.path, "w", encoding="utf-8") as file:
-            json.dump(old_vacancies, file)
+        self.write_json(old_vacancies)
 
     def get_vacancies(self, queries=None) -> list[dict]:
-        with open(self.path, "r", encoding="utf-8") as file:
-            all_vacancies: list[dict] = json.load(file)
+        all_vacancies = self.read_json()
         vacancies = []
         for vacancy in all_vacancies:
             if all(vacancy.get(field) == query for field, query in queries.items()):
@@ -44,11 +51,9 @@ class FileSaverJSON(FileSaver):
 
     def del_vacancies(self, queries=None) -> None:
         vacancies_to_del = self.get_vacancies(queries)
-        with open(self.path, "r", encoding="utf-8") as file:
-            all_vacancies: list[dict] = json.load(file)
+        all_vacancies = self.read_json()
         vacancies = []
         for vacancy in all_vacancies:
             if vacancy not in vacancies_to_del:
                 vacancies.append(vacancy)
-        with open(self.path, "w", encoding="utf-8") as file:
-            json.dump(vacancies, file)
+        self.write_json(vacancies)
