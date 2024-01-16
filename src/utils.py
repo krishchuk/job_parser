633606d38ba9a960362a715a31d77f0b347ed68d
denis krishchuk <ms.den.best@mail.ru>
2024-01-17 01:35:12 +0500

    add try/except for TypeErrors in "create instances" funcs

--- src/utils.py
+++ src/utils.py
@@ -4,34 +4,54 @@ from src.jobs import HeadHunterVacancy, SuperJobVacancy, Vacancy
 def create_hh_instances(vacancies: list[dict]) -> list[Vacancy]:
     all_vacancies = []
     for vacancy in vacancies:
-        sj_vacancy = HeadHunterVacancy(
+        try:
+            salary_from = vacancy["salary"]["from"]
+        except TypeError:
+            salary_from = 0
+        try:
+            salary_to = vacancy["salary"]["to"]
+        except TypeError:
+            salary_to = 0
+        try:
+            currency = vacancy["salary"]["currency"]
+        except TypeError:
+            currency = ""
+        try:
+            town = vacancy["area"]["name"]
+        except TypeError:
+            town = ""
+        hh_vacancy = HeadHunterVacancy(
             vacancy_name=vacancy["name"],
-            salary_from=vacancy["salary"]["from"],
-            salary_to=vacancy["salary"]["to"],
-            currency=vacancy["salary"]["currency"],
-            town=vacancy["area"]["name"],
+            salary_from=salary_from,
+            salary_to=salary_to,
+            currency=currency,
+            town=town,
             url=vacancy["url"]
         )
-        all_vacancies.append(sj_vacancy)
+        all_vacancies.append(hh_vacancy)
     return all_vacancies
 
 
 def create_sj_instances(vacancies: list[dict]) -> list[Vacancy]:
     all_vacancies = []
     for vacancy in vacancies:
+        try:
+            town = vacancy["town"]["title"]
+        except TypeError:
+            town = ""
         sj_vacancy = SuperJobVacancy(
             vacancy_name=vacancy["profession"],
             salary_from=vacancy["payment_from"],
             salary_to=vacancy["payment_to"],
             currency=vacancy["currency"],
-            town=vacancy["town"]["title"],
+            town=town,
             url=vacancy["link"]
         )
         all_vacancies.append(sj_vacancy)
     return all_vacancies
 
 
-def sort_by_salary(vacancies: list[Vacancy]) -> list[Vacancy]:
+def sort_by_salary(vacancies: list) -> list[Vacancy]:
     return sorted(vacancies, reverse=True)
 
 
